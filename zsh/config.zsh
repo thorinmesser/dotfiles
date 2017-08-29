c55b3d7d5f903ccfd6470556a144ad4312da8360
Thorin Messer <thorin.messer@vml.com>
2017-08-29 12:12:54 -0400

    Prefer old pure so I can use git-radar without spending three days figuring out how to integrate it with the new pure stuff.

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -9,10 +9,6 @@
 export LSCOLORS="exfxcxdxbxegedabagacad"
 export CLICOLOR=true
 
-fpath=($ZSH/functions $fpath)
-
-autoload -U $ZSH/functions/*(:t)
-
 HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
